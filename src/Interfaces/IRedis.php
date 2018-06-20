314ea6cde8b63928d0e0f7b7a45a2b2758b1cce3
Joao Moraes <joaomoraesbr@gmail.com>
2018-06-19 22:56:01 -0300

    add interface methods

--- src/Interfaces/IRedis.php
+++ src/Interfaces/IRedis.php
@@ -2,8 +2,14 @@
 
 namespace FcPhp\Redis\Interfaces
 {
+	use Redis as RedisPHP;
+	
 	interface IRedis
 	{
-		
+		public function __construct(RedisPHP $redis, string $host, string $port, string $password = null, int $timeout = 100);
+
+		public function get(string $key);
+
+		public function set(string $key, $content) :IRedis;
 	}
 }
